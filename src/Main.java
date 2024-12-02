0eb7289558279fa482d26c3751a72a45a5a3555c
SoraHrS <soherreras@unal.edu.co>
2024-12-02 15:52:23 -0500

    se realizó el ejercicio 11

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        double numero1, numero2, numero3, mayor;
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        System.out.println("Introduzca el primer numero: ");
+        numero1 = sc.nextDouble();
+        System.out.println("Introduzca el segundo numero: ");
+        numero2 = sc.nextDouble();
+        System.out.println("Introduzca el tercer numero: ");
+        numero3 = sc.nextDouble();
 
-        System.out.println("Hello world!");
+        if (numero1 > numero2 && numero1 > numero3) {
+            mayor = numero1;
+        }else if (numero2 > numero1 && numero2 > numero3) {
+            mayor = numero2;
+        }else{
+            mayor = numero3;
+        }
+        System.out.println("El mayor numero entre: " + numero1 + " , " + numero2 + " y " + numero3 + " es " + mayor);
     }
 }
